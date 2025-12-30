ff309e737e73ceff824c890791b3d0be174720ab
Marc Lehmann <marc@navisense.de>
2025-12-30 20:18:04 +0100

    Test call formatter separately

--- yapftests/format_test.py
+++ yapftests/format_test.py
@@ -8,10 +8,13 @@ from yapf.yapflib import style
 from yapftests import yapf_test_helper
 
 
-class ModuleFormatterTest(yapf_test_helper.YAPFTest):
+class FormatterTest(yapf_test_helper.YAPFTest):
 
   def setUp(self):  # pylint: disable=g-missing-super-call
     self.config = style.CreatePEP8Style()
+
+
+class ModuleFormatterTest(FormatterTest):
 
   def _Check(self, unformatted_code, expected_formatted_code):
     module = cst.parse_module(unformatted_code)
@@ -93,11 +96,37 @@ class ModuleFormatterTest(yapf_test_helper.YAPFTest):
     """)
     self._Check(unformatted_code, expected_formatted_code)
 
+
+class CallFormatterTest(FormatterTest):
+
+  def _Check(self, unformatted_code, expected_formatted_code):
+    module = cst.parse_module(unformatted_code)
+    assert len(module.body) == 1
+    line = module.body[0]
+    assert len(line.body) == 1 and isinstance(line, cst.SimpleStatementLine)
+    expr = line.body[0]
+    assert isinstance(expr, cst.Expr)
+    call = expr.value
+    assert isinstance(call, cst.Call)
+    formatter = format.CallFormatter(module, self.config)
+    formatted_call = call.visit(formatter)
+    formatted_module = module.deep_replace(call, formatted_call)
+    self.assertCodeEqual(expected_formatted_code, formatted_module.code)
+
   def testRemovesWhitespaceBeforeArgsList(self):
     unformatted_code = textwrap.dedent("""\
         function_name ()
     """)
     expected_formatted_code = textwrap.dedent("""\
-       function_name()
+        function_name()
+    """)
+    self._Check(unformatted_code, expected_formatted_code)
+
+  def testRemovesExtraneousWhitespaceAfterStars(self):
+    unformatted_code = textwrap.dedent("""\
+        function_name(*  args, **  kwargs)
+    """)
+    expected_formatted_code = textwrap.dedent("""\
+        function_name(*args, **kwargs)
     """)
     self._Check(unformatted_code, expected_formatted_code)
